f457936a5b012d23f338f24a577a53f635b10a96
iris_ <iris_starry_@naver.com>
2023-04-15 22:27:37 +0900

    피보나치 수열에 대하여 100번째 항까지의 합을 구하는 알고리즘

---
+++ Algorithms/Q5.c
@@ -0,0 +1,18 @@
+// 피보나치 수열에 대하여 100번째 항까지의 합을 구하는 알고리즘
+// 1 + 1 + 2 + 3 + 5 + 8 + 13
+
+#include <stdio.h>
+main() {
+	int a, b, c, s, n;
+	a = 1; b = 1; s = 2; n = 2;
+	do {
+		c = a + b;
+		s = s + c;
+		n = n + 1;
+		if (n == 100) break;
+		a = b;
+		b = c;
+	} while (1);
+
+	printf("total = %d입니다.\n", s);
+}
